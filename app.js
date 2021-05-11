59e4901e05ccb212386665543173d97db3491b8c
saumya611 <55658827+saumya611@users.noreply.github.com>
2021-05-11 17:16:31 +0530

    add start script

--- app.js
+++ app.js
@@ -124,6 +124,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error.ejs', { err});
 });
 
-app.listen(3000, () => {
-    console.log("APP IS Listening on port 3000!");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`APP IS Listening on port ${port}!`);
 });
