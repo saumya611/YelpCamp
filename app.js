41ab1189bdd7e3da2d53371bea949539bc948642
saumya611 <55658827+saumya611@users.noreply.github.com>
2021-05-17 14:14:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,128 @@
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError.js');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+
+const User = require('./models/user');
+const userRoutes = require('./routes/users.js');
+const campgroundRoutes = require('./routes/campgrounds.js');
+const reviewRoutes = require('./routes/reviews.js');
+
+// const catchAsync = require('./utils/catchAsync.js');
+
+const MongoStore = require("connect-mongo");
+
+// const joi = require('joi');
+// const { ppid } = require('process');
+
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+
+// 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+// const { func } = require('joi');
+
+const secret = process.env.SECRET || "it's my secret i'll not gonna tell you";
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: secret
+    },
+    touchAfter: 24 * 60 * 60
+});
+
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+});
+
+const sessionConfig = {
+    store,
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // By Above options third party can't access cookies 
+        expires: Date.now() + 1000 * 60 *60 *24 *7,
+        maxAge: 1000 * 60 *60 *24 *7
+    }
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    console.log("In the main app.js");
+    console.log(req.session);
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next(); 
+});
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+
+app.get('/', (req, res) => { 
+    res.render("home");
+});
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode =500} = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`APP IS Listening on port ${port}!`);
+});
---
+++ middleware.js
@@ -0,0 +1,61 @@
+const ExpressError = require('./utils/ExpressError')
+const Campground = require('./models/campground')
+const Review = require('./models/review.js')
+const catchAsync = require('./utils/catchAsync.js');
+const { campgroundSchema, reviewSchema} = require('./schemas.js');
+
+module.exports.isLoggedIn = (req, res, next) => {
+    console.log("In The Middleware");
+    if(!req.isAuthenticated()){
+        console.log("REQ.USER....", req.user);
+        // console.log(req.path, req.originalUrl);
+        req.session.returnTo = req.originalUrl;
+        req.flash('error', 'you must be signed in first');
+        return res.redirect('/login');
+    }
+    next();
+};
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    console.log("In The Middleware");
+    console.log(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(', ');
+        next(new ExpressError(msg, 400));
+    } else{
+        next();
+    }
+};
+
+module.exports.isAuthor = async(req, res, next) => {
+    const { id } = req.params;
+    const campground= await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    console.log("In The Middleware");
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
+
+module.exports.validateReview = (req, res, next) => {
+    console.log("In The Middleware");
+    const { error} = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        next(new ExpressError(msg, 400));
+    } else{
+        next();
+    }
+};
