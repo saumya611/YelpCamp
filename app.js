058661d0c7378a83ef51356f2fb0ff0157875f04
saumya611 <55658827+saumya611@users.noreply.github.com>
2021-05-21 16:19:09 +0530

    ready after security and map issue 2nd

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ const MongoStore = require("connect-mongo");
 // const joi = require('joi');
 // const { ppid } = require('process');
 
-// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
 // 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
